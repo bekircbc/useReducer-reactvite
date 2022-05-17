60c216514dd2b61185351778ed95ae61738b9295
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-17 10:26:54 +0200

    added useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-import "./App.css";
+import { useReducer } from "react";
+import "./App.scss";
+
+const initialState = {
+  number: 0,
+  lastAction: "",
+  whenLastClicked: "",
+  timeClicked: 0,
+};
+
+const reducer = (theState, action) => {
+  const obj = {};
+  obj.whenLastClicked = String(new Date());
+  obj.timesClicked = theState.timesClicked + 1;
+  switch (action.type) {
+    case "down":
+      obj.number = theState.number - 1;
+      obj.lastAction = "decrement";
+      break;
+    case "up":
+      obj.number = theState.number + 1;
+      obj.lastAction = "increment";
+      break;
+  }
+  return obj;
+};
 
 function App() {
-  return <div className="App">Hello!</div>;
+  return (
+    <div className="App">
+      <h1>useReducer</h1>
+      <div>Number: {state.number}</div>
+      <div>Last action: {state.lastAction}</div>
+      <div className="when">When last clicked: {state.whenLastClicked}</div>
+      <div className="timesClicked">Times clicked: {state.timesClicked}</div>
+      <div className="buttonArea">
+        <button onClick={() => dispatch({ type: "down" })}>-</button>
+        <button onClick={() => dispatch({ type: "up" })}>+</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
